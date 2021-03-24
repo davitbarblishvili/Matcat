60ea7c146d30af791ebef5eff638f23d5f979484
Davit Barblishvili <db3230@columbia.edu>
2021-03-24 19:50:16 -0400

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -118,15 +118,13 @@ let string_of_vdecl (t, id) = string_of_data_type t ^ " " ^ id ^ ";\n"
 
 let string_of_fdecl fdecl =
   "func " ^
-  fdecl.fname ^
-  "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ") " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ") "^
   String.concat "" (List.map string_of_data_type fdecl.data_types) ^
-  " {\n" ^
+  "{\n" ^
   String.concat "" (List.map string_of_vdecl fdecl.locals) ^
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "}\n"
-
 let string_of_program (vars, funcs) =
   String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
   String.concat "\n" (List.map string_of_fdecl funcs)
