7694538840da907a004cdd22bc34127e485b47b4
Andreas <and@reas.me>
2021-04-12 18:37:12 +0800

    Refine coding environment.
    .merlin:
    For Ocaml's static analysis (better error messages).
    Partially working (less irrelevant errors in codegen).
    To make this works better, we may need to restructure our repo
    and redefine .merlin.
    
    run.sh:
    Read desc.

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Author:       Andreas
+# Usage:        ./mc.sh <filename>
+# Description:  This shell script compiles and runs a single .mc file to test a single test case faster
+
+LLI="lli"
+LLC="llc"
+CC="cc"
+MATCAT="./matcat.native"
+ulimit -t 5
+
+SignalError() {
+    if [ $error -eq 0 ] ; then
+	echo "FAILED"
+	error=1
+    fi
+    echo "  $1"
+}
+
+Run() {
+    echo $* 1>&2
+    eval $* || {
+	SignalError "$1 failed on $*"
+	return 1
+    }
+}
+
+basename=`echo $1 | sed 's/.*\\///
+                         s/.mc//'`
+reffile=`echo $1 | sed 's/.mc$//'`
+basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
+
+Run $MATCAT $1 > $basename.ll &&
+Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
+Run "$CC" "-o" "${basename}.exe" "${basename}.s"  &&
+Run ./$basename.exe
+
+rm -f "$basename.exe"
+rm -f "$basename.s"
+rm -f "$basename.ll"
