e2ba42df982f3aaf17b9e3fd4829bf2af4d53a3c
Andreas <and@reas.me>
2021-04-25 03:53:54 +0800

    minor fix in sast

--- sast.ml
+++ sast.ml
@@ -70,6 +70,7 @@ let rec string_of_sexpr (t, e) =
     | SCall(f, el) ->
         f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
     | SNoexpr -> ""
+    | SNoassign -> ""
   )
   ^ ")"		
 
